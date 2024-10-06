7fec59084f935c9c0e225ffdc3330584b8bd5410
Rokshana Kaiser <rokshanakaiser@Rokshanas-MacBook-Pro.local>
2024-10-06 23:53:27 +0600

    math project

---
+++ main.js
@@ -0,0 +1,56 @@
+// 1st math
+let AppleAse =25;
+let AroKinlo =15;
+
+let MotApple =AppleAse + AroKinlo;
+
+console.log("Mot apple ase:" + MotApple + "ti")
+
+// 2nd math
+let TakaAse =500;
+let Khoroch =200;
+
+let Ache = TakaAse - Khoroch;
+
+console.log("leftover money:" + Ache + "taka")
+
+// 3rd math
+let PerHourProduction = 120;
+let TotalHour =8;
+
+let TotalProduction = PerHourProduction * TotalHour;
+
+console.log("engine ache:" + TotalProduction + "ti")
+// 4th math
+let OneRow = 12;
+let TotalRow = 8;
+let Added = 20;
+
+let TotalPlant = OneRow * TotalRow + Added;
+
+console.log("plant ache:" + TotalPlant + "ti")
+// 5th math
+let PerBox = 10;
+let TotalBox = 5;
+let Decrease = 12;
+
+let RahimHave = PerBox * TotalBox - Decrease;
+
+console.log("komola ache:" + RahimHave + "ti")
+// 6th math
+let Distance = 150;
+let Hour = 3;
+let PerHourDistance = Distance / Hour;
+
+console.log("per hour distance now:" + PerHourDistance + "hour")
+// 7th math
+let Productio = 200;
+let UseEngine = 6;
+let TotalEngine = 8;
+let Left = 3;
+
+let PerEngineProduction = Productio / UseEngine;
+let Production = PerEngineProduction * TotalEngine;
+let LeftProduction = Production - Left;
+
+console.log("left over engine:" + LeftProduction + "ti")
